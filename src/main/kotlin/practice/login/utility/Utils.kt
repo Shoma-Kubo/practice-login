802cb4e9e4cf72670bdc6359fb1ec3622f25b848
Shoma Kubo <shoma.kubo.1800@gmail.com>
2022-06-09 11:31:01 +0900

    Base64Encode の修正．

--- src/main/kotlin/practice/login/utility/Utils.kt
+++ src/main/kotlin/practice/login/utility/Utils.kt
@@ -1,10 +1,18 @@
 package practice.login.utility
 
+import java.nio.ByteBuffer
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import java.util.*
 
 object Utils {
+
+  fun UUID.toBase64URLSafeStringFromUUID(): String {
+    val bb: ByteBuffer = ByteBuffer.wrap(ByteArray(size = 16))
+    bb.putLong(this.mostSignificantBits)
+    bb.putLong(this.leastSignificantBits)
+    return org.apache.tomcat.util.codec.binary.Base64.encodeBase64URLSafeString(bb.array())
+  }
 
   fun String.toBase64URLSafeString(): String =
     Base64.getEncoder().encodeToString(this.toByteArray())
